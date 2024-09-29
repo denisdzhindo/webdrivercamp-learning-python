710565a15dc035d41cb9a10996b136ea4abc967c
Denis Dzhindo <santacruz@deniss-mbp.lan>
2024-09-29 00:09:49 -0700

    the second one

---
+++ python_beginning/1_adding_some_numbers.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+one_hundred = 100
+
+print(f"Learning python is fun\"' - {one_hundred} %") 
+print("Learning python is fun\"' - {} %".format(one_hundred))
+print("Learning python is fun\"' - %s" % (one_hundred), "%")
