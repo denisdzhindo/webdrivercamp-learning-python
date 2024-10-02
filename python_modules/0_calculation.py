7be6e8fe9a658bf067cfe5e2971b535352f9e0ad
Denis Dzhindo <santacruz@deniss-mbp.lan>
2024-10-01 23:03:12 -0700

    2.2.0

---
+++ python_modules/0_calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__=="__main__":
+
+  #ADD YOUR IMPORT HERE
+ import calculation
+
+ a = 4 #use this variable as a first argument to call a function 
+ b = 2 #use this variable as a second argument to call a function
+  #ADD YOUR CODE HERE
+
+
+
+ print(f"4 + 2 = {calculation.add(a, b)}")
+ print(f"4 - 2 = {calculation.sub(a, b)}")
+ print(f"4 * 2 = {calculation.mul(a, b)}")
+ print(f"4 / 2 = {calculation.div(a, b)}")
+  
+
