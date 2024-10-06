8ec4b1dc0ceac7f071fe33cda1bdbd71e6ba5951
Denis Dzhindo <santacruz@deniss-mbp.lan>
2024-10-05 17:40:47 -0700

    adding 2.3 8. When returns more

--- python_data_structures/8_tuple_return.py
+++ python_data_structures/8_tuple_return.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+
+def tuple_return(list_):
+    if len(list_) == 0:
+        first_element = None
+    else:
+        first_element = list_[0]
+
+    return len(list_), first_element
+
 
