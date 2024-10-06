21382ff8eed9cbcb687c09152bd6b80c62a39283
Denis Dzhindo <santacruz@deniss-mbp.lan>
2024-10-05 22:53:41 -0700

    2.1 300. Which one is the last one?

---
+++ python_loops_and_more/300_even_or_odd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import random
+
+random_num = random.randint(-10000, 10000)
+last_digit = abs(random_num) % 10
+if random_num < 0:
+  last_digit *= -1
+
+
+if last_digit == 0:
+  print(f"{random_num} - the last digit {last_digit} is zero")
+elif last_digit % 2 == 0:
+  print(f"{random_num} - the last digit {last_digit} is even")
+else:
+  print(f"{random_num} - the last digit {last_digit} is odd")
+
