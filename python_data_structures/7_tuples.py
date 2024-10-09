30d5ad2d536207a2dee5db09dbd13888b98727d8
denisdzhindo <dzhindo@outlook.com>
2024-10-08 21:39:52 -0700

    Update 7_tuples.py

--- python_data_structures/7_tuples.py
+++ python_data_structures/7_tuples.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-def tuple_addition(first_arg = None, second_arg = None):
+def tuple_addition(first_arg = (), second_arg = ()):
 
-  if first_arg == None:
-    first_arg = (0, 0)
-  if second_arg == None:
-    second_arg = (0, 0)
   if first_arg == ():
     first_arg = (0, 0)
   if second_arg == ():
