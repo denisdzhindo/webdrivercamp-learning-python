d1fcdad28410f1d30a93dab94c7b7347e1c6cea0
denisdzhindo <dzhindo@outlook.com>
2024-10-14 21:50:22 -0700

    Create 0_compute_matrix.py 4.1.0
    This project is continuing with Python Core Data Structures

---
+++ python-data-structures-continue/0_compute_matrix.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def compute_matrix(matrix=[]):
+    # YOUR CODE HERE
+if __name__=="__main__":
+    matrix = [
+            [9, 8, 7],
+            [6, 5, 4],
+            [3, 2, 1]
+        ]
+    print(f"Original: {matrix}")
+    print(f"Modified: {compute_matrix(matrix)}")
