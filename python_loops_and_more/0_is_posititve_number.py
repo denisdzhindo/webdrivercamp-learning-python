73cb932b448152522660407af6e19a42863e3e47
denisdzhindo <dzhindo@outlook.com>
2024-10-15 23:52:05 -0700

    Update 0_is_posititve_number.py

--- python_loops_and_more/0_is_posititve_number.py
+++ python_loops_and_more/0_is_posititve_number.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
 import random
+
 random_num = random.randint(-10, 10)
 
 if random_num > 0:
   print(f"{random_num} is positive")
-
 elif random_num < 0:
   print(f"{random_num} is negative")
-
 else:
   print("0 is zero")
